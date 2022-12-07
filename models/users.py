381db63c8cbb87af0793fd7939d8d759c751c518
Allan Williams Musembya <engallanmusembya@gmail.com>
2022-12-07 21:59:53 +0300

    updates

--- models/users.py
+++ models/users.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class res_user(models.Model):
     _inherit = 'res.partner'
     
-    user_role = fields.Selection([('infrastructure_admin', 'Infrastructure Admin'),('infrastructure_manager', 'Infrastructure Manager')], string="User Role")
+    user_roles = fields.Selection([('infrastructure_admin', 'Infrastructure Admin'),('infrastructure_manager', 'Infrastructure Manager')], string="User Role")
     supervisor = fields.Many2one('res.partner',string ='Supervisor')
     loaction_id_invetory = fields.Many2one('inventory_track.asset_location',string ='Location')
 
