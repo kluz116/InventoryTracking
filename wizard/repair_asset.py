8fb28e69c9c004800b93c404231f4ef18782ee19
Allan Williams Musembya <engallanmusembya@gmail.com>
2023-01-15 01:13:44 +0300

    dfdfdfffdfdf

--- wizard/repair_asset.py
+++ wizard/repair_asset.py
@@ -11,8 +11,25 @@ class RepairAsset(models.TransientModel):
     repaire_comment = fields.Text(string="Comment", required=True)
     repaire_date =  fields.Datetime(string='Repair Date', default=lambda self: fields.datetime.now())
     repaired_by = fields.Many2one('res.users','Initiated By:',default=lambda self: self.env.user)
-    tag = fields.Char(string="Asset TAG", required=True,compute="comp_asset_tag")
-   
+
+    def comp_asset_tag(self):
+        asset = self.env['inventory_track.inventory'].browse(self._context.get('active_ids'))
+        for req in asset:
+            return req.tag 
+
+    tag = fields.Many2one('inventory_track.asset_tags',string="Asset TAG",default=comp_asset_tag, required=True)
+    serial_set =   fields.Many2many(related='tag.asset_serial',ondelete='cascade',string='Asset Serial')
+    
+    def comp_asset_serial(self):
+        asset = self.env['inventory_track.inventory'].browse(self._context.get('active_ids'))
+        for req in asset:
+            return req.tag.asset_serial
+
+    serial = fields.Many2many('inventory_track.asset_serial',default=comp_asset_serial,string='Asset Serial',domain = " [('status','in',[serial_set])] ")
+    #serial_set =   fields.Many2many(related='tag.asset_serial',ondelete='cascade',string='Asset Serial')
+
+    
+
     
     def repair_asset(self):
         self.write({'asset_status': 'repair'})
@@ -38,9 +55,5 @@ class RepairAsset(models.TransientModel):
             #template.send_mail(req.id,force_send=True)
          
 
-       
-    #@api.depends('start_date')
-    def comp_asset_tag(self):
-        asset = self.env['inventory_track.inventory'].browse(self._context.get('active_ids'))
-        for req in asset:
-            self.tag = req.tag.tag 
+
+
