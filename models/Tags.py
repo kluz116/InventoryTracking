7a3ca9d1682a4ad7eeb13839bf8b4a3d8f267bfe
Allan Williams Musembya <engallanmusembya@gmail.com>
2023-01-18 09:55:00 +0300

    Update Tags.py

--- models/Tags.py
+++ models/Tags.py
@@ -21,9 +21,9 @@ class Tag(models.Model):
     status =  fields.Selection([('active','Active'),('innactive','Innactive')],string="Status", required=True, default="innactive")
     tag_date =  fields.Datetime(string='Created Date', default=lambda self: fields.datetime.now())
     created_by = fields.Many2one('res.users','Created By:',default=lambda self: self.env.user)
-    base_url = fields.Char('Base Url', compute='_get_url_id', store='True')
+    
 
-    @api.depends('tag_date')
+    #@api.depends('tag_date')
     def _get_url_id(self):
         for rec in self:
             web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
@@ -31,4 +31,4 @@ class Tag(models.Model):
             rec.base_url = """{}/web#id={}&view_type=form&model=inventory_track.asset_tags&action={}""".format(web_base_url,rec.id,action_id.id)
 
    
-    
+    base_url = fields.Char('Base Url', default='_get_url_id')
